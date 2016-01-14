534a7e836718bceb7db5f070f7ecb2f3828b9489
Christoph Olszowka <christoph@olszowka.de>
2016-01-14 16:44:29 +0100

    Do not use period in pubkey filename, skvs does not allow it

--- lib/publish_to_web/config.rb
+++ lib/publish_to_web/config.rb
@@ -24,8 +24,8 @@ class PublishToWeb
     config_attribute :hardware_id, "ptw/hardware_id"
     config_attribute :license_key, "ptw/license"
     config_attribute :node_name,   "hostname"
-    config_attribute :private_key, "ptw/publish_to_web_key"
-    config_attribute :public_key,  "ptw/publish_to_web_key.pub"
+    config_attribute :private_key, "ptw/publish_to_web_key_private"
+    config_attribute :public_key,  "ptw/publish_to_web_key_public"
     config_attribute :success,     "ptw/success"
     config_attribute :error,       "ptw/error"
   end
--- spec/publish_to_web/config_spec.rb
+++ spec/publish_to_web/config_spec.rb
@@ -8,8 +8,8 @@ describe PublishToWeb::Config do
     hardware_id: 'ptw/hardware_id',
     license_key: 'ptw/license',
     node_name:   'hostname',
-    private_key: 'ptw/publish_to_web_key',
-    public_key:  'ptw/publish_to_web_key.pub',
+    private_key: 'ptw/publish_to_web_key_private',
+    public_key:  'ptw/publish_to_web_key_public',
     success:     'ptw/success',
     error:       'ptw/error'
   }.each do |method_name, store_key|
